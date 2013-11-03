5206fe12173f47976e37d0b7deae220d604fb63e
Márcio Martins <marciomartins.it@gmail.com>
2013-11-03 22:24:41 +0000

    update rdf

--- Scripts/generateRDF.js
+++ Scripts/generateRDF.js
@@ -1,9 +1,20 @@
 function generateRDFa(book) {
-    tagHTML = document.getElementsByName("html")[0];
-    tagHTML.setAttributeNS("xmlns:ed", "http://www.dei.isep.ipp.pt/arqsi/editoras#");
-    tagHTML.setAttributeNS("xmlns:rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
-    tagHTML.setAttributeNS("xmlns:h", "http://www.w3.org/1999/xhtml");
-    tagHTML.setAttributenS("xmlns", "http://www.w3.org/1999/xhtml");
+    
+    tagDiv = document.getElementById("divRDF");
+    
+    tagSpanTitulo = document.createElement("span");
+    tagSpanTitulo.setAttribute("property","dc:title");
+    txtTitle = document.createTextNode(book.childNodes[0].textContent);
+    tagSpanTitulo.appendChild(txtTitle);
+    tagDiv.appendChild(tagSpanTitulo);
+    
+    /*
+    tagHTML = document.getElementsByTagName("html")[0];
+    
+    tagHTML.setAttribute("xmlns:ed", "http://www.dei.isep.ipp.pt/arqsi/editoras#");
+    tagHTML.setAttribute("xmlns:rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+    tagHTML.setAttribute("xmlns:h", "http://www.w3.org/1999/xhtml");
+    tagHTML.setAttribute("xmlns", "http://www.w3.org/1999/xhtml");
 
 
     var i = 1;
@@ -12,36 +23,46 @@ function generateRDFa(book) {
     tagSpan.setAttribute("typeof", "ed:book");
     tagSpan.setAttribute("resource", "#book1." + i);
 
+    */
+/*
     ulTagTitulo = document.createElement("ul");
-    ulTagTitulo.setAttribute("property", "ed:title");
-    ulTxtNodeTitulo = document.createElement(book.childNodes[0].nodeValue);
-    ulTagTitulo.appendChild(ulTxtNodeTitulo);
+    spanTitulo = document.createElement("span");
+    
+    spanTitulo.setAttribute("property", "ed:title");
+    ulTxtNodeTitulo = document.createTextNode(book.childNodes[0].textContent);
+    spanTitulo.appendChild(ulTxtNodeTitulo);
+    ulTagTitulo.appendChild(spanTitulo);
     tagSpan.appendChild(ulTagTitulo);
 
     ulTagAuthor = document.createElement("ul");
-    ulTagAuthor.setAttribute("property", "ed:author");
-    ulTxtNodeAuthor = document.createElement(book.childNodes[1].nodeValue);
-    ulTagAuthor.appendChild(ulTxtNodeAuthor);
+    spanAuthor = document.createElement("span");
+    
+    spanAuthor.setAttribute("property", "ed:author");
+    ulTxtNodeAuthor = document.createTextNode(book.childNodes[1].textContent);
+    spanAuthor.appendChild(ulTxtNodeAuthor);
+    ulTagAuthor.appendChild(spanAuthor)
     tagSpan.appendChild(ulTagAuthor);
     
+    */
+    /*
     ulTagCategory = document.createElement("ul");
     ulTagCategory.setAttribute("property", "ed:category");
-    ulTxtNodeCategory = document.createElement(book.childNodes[2].nodeValue);
+    ulTxtNodeCategory = document.createTextNode(book.childNodes[2].textContent);
     ulTagCategory.appendChild(ulTxtNodeCategory);
     tagSpan.appendChild(ulTagCategory);
     
     ulTagIsbn = document.createElement("ul");
     ulTagIsbn.setAttribute("property", "ed:isbn");
-    ulTxtNodeIsbn = document.createElement(book.childNodes[3].nodeValue);
+    ulTxtNodeIsbn = document.createTextNode(book.childNodes[3].textContent);
     ulTagIsbn.appendChild(ulTxtNodeIsbn);
     tagSpan.appendChild(ulTagIsbn);
     
     ulTagPublicacao = document.createElement("ul");
     ulTagPublicacao.setAttribute("property", "ed:publicacao");
-    ulTxtNodePublicacao = document.createElement(book.childNodes[4].nodeValue);
+    ulTxtNodePublicacao = document.createTextNode(book.childNodes[4].textContent);
     ulTagPublicacao.appendChild(ulTxtNodePublicacao);
     tagSpan.appendChild(ulTagPublicacao);
+    */
     
-    divRDF.appendChild(tagSpan);
         
 }
